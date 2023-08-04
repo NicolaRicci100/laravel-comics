fdd37ff207bafccbbbc0ac32421e1e02ce93b5b0
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-04 11:55:32 +0200

    stile per l'header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }} | Home</title>
+    <link rel="icon" href="{{ Vite::asset('resources/images/dc-logo.png') }}" type="images/png">
     @vite('resources/js/app.js')
 </head>
 
 <body>
-    <h1>Prova</h1>
+    <header>
+        <nav class="container">
+            <figure>
+                <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo DC">
+            </figure>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Collectibles</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">Fans</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </body>
 
 </html>
