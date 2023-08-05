7179850d9cda26968c6dae690517dc8f3ab09abc
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-05 11:40:54 +0200

    menu dinamico

---
+++ config/main_menu.php
@@ -0,0 +1,53 @@
+<?php
+return [
+  [
+    "text" => "Characters",
+    "url" => "/characters",
+    "route" => "characters",
+  ],
+  [
+    "text" => "Comics",
+    "url" => "/comics",
+    "route" => "comics",
+  ],
+  [
+    "text" => "Movies",
+    "url" => "/movies",
+    "route" => "movies",
+  ],
+  [
+    "text" => "TV",
+    "url" => "/tv",
+    "route" => "tv",
+  ],
+  [
+    "text" => "Games",
+    "url" => "/games",
+    "route" => "games",
+  ],
+  [
+    "text" => "Collectibles",
+    "url" => "/collectibles",
+    "route" => "collectibles",
+  ],
+  [
+    "text" => "Videos",
+    "url" => "/videos",
+    "route" => "videos",
+  ],
+  [
+    "text" => "Fans",
+    "url" => "/fans",
+    "route" => "fans",
+  ],
+  [
+    "text" => "News",
+    "url" => "/news",
+    "route" => "news",
+  ],
+  [
+    "text" => "Shop",
+    "url" => "/shop",
+    "route" => "shop",
+  ],
+];
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,39 +1,18 @@
+@php
+    $links = config('main_menu');
+@endphp
+
 <header>
     <nav class="container">
         <figure>
             <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo DC">
         </figure>
         <ul>
-            <li>
-                <a href="#">Characters</a>
-            </li>
-            <li>
-                <a href="#">Comics</a>
-            </li>
-            <li>
-                <a href="#">Movies</a>
-            </li>
-            <li>
-                <a href="#">TV</a>
-            </li>
-            <li>
-                <a href="#">Games</a>
-            </li>
-            <li>
-                <a href="#">Collectibles</a>
-            </li>
-            <li>
-                <a href="#">Videos</a>
-            </li>
-            <li>
-                <a href="#">Fans</a>
-            </li>
-            <li>
-                <a href="#">News</a>
-            </li>
-            <li>
-                <a href="#">Shop</a>
-            </li>
+            @foreach ($links as $link)
+                <li>
+                    <a href="#">{{ $link['text'] }}</a>
+                </li>
+            @endforeach
         </ul>
     </nav>
 </header>
