d9d778eed29811fb6e3975221c3b1aa6d9c46a07
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-21 15:51:10 +0200

    stile miniatura concluso

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,6 +4,12 @@
     <section id="single-comic">
         <div class="large-jumbo">
             <figure class="figure-comic">
+                <div class="comic-label label-top">
+                    comic book
+                </div>
+                <div class="comic-label label-down">
+                    view gallery
+                </div>
                 <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
             </figure>
         </div>
